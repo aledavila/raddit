d03a3658ca551c67237db0be103f54d9c7b18f25
Alessandra Davila <alex.davila@utexas.edu>
2016-10-10 17:33:54 -0500

    adds posts and comments routes

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/news', function(err,db){
+    if (!err){
+        console.log('Connected to /news!');
+    } else{
+        console.dir(err); //failed to connect
+    }
+});
+
+require('./models/Posts');
+require('./models/Comments');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
